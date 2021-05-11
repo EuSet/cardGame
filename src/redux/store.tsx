494bf8ea65a7659355433bb43c6e77074de34441
EuSet <setskoeugene@inbox.ru>
2021-05-11 18:48:05 +0300

    added localStorage

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,9 +2,16 @@ import {applyMiddleware, combineReducers, createStore} from "redux"
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {playReducer} from "./play-reducer";
+import {loadState, saveState} from "../Components/Utills/localStorage";
 
 let reducers = combineReducers({
     playPage: playReducer,
 })
 export type StateType = ReturnType<typeof reducers>
-export let store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)) )
+export let store = createStore(reducers, loadState(), composeWithDevTools(applyMiddleware(thunk)) )
+
+store.subscribe(() => {
+    saveState({
+        playPage: store.getState().playPage
+    })
+})
